ae834884fd2ac411c55a360883f08d2de8314c9f
Jubin Park <jubin-park@naver.com>
2019-02-15 23:30:39 +0900

    Revise Button

--- button.rb
+++ button.rb
@@ -1,32 +1,40 @@
 class Button
 
-	attr_accessor :x, :y, :width, :height
-	attr_writer :caption
-	attr_reader :window
-	attr_accessor :gimage_caption
+	attr_reader		:window
+	attr_accessor :x, :y, :width, :height, :z
+	attr_writer		:caption
+	attr_accessor :gi_caption
 
-	def initialize(window, width, height, caption="")
+	def initialize(window, width, height, caption=nil)
 	    @window = window
-	    @width, @height = width, height
 	    @x, @y = 0, 0
+	    @width, @height = width, height
+	    @z = 0
 	    @caption = caption
-	    @gimage_caption = Gosu::Image.from_text(@caption, height * 0.6)
+	    @image = Array.new(2)
+	    if !@caption.nil?
+	    	@gi_caption = Gosu::Image.from_text(@caption, height * 0.6)
+	    end
 	    ObjectSpace.define_finalizer(self, self.class.method(:finalize))
+	end
+
+	def set_image(index, gi)
+		@image[index] = gi
+	end
+
+	def draw
+		if !under_mouse?
+			@image[0].draw(@x, @y, @z)
+		else
+			@image[1].draw(@x, @y, @z)
+		end
+		if !@gi_caption.nil?
+			@gi_caption.draw(@x + (@width - @gi_caption.width) / 2, @y + (height - @gi_caption.height) / 2, @z + 1)
+		end
 	end
 
 	def under_mouse?
 		return @window.mouse_x >= @x && @window.mouse_x < @x + @width && @window.mouse_y >= @y && @window.mouse_y < @y + @height
-	end
-
-	def draw(z)
-		if under_mouse?
-			#Gosu.draw_rect(@x, @y, @width, @height, Gosu::Color::CYAN)
-		else
-			#Gosu.draw_rect(@x, @y, @width, @height, Gosu::Color::GRAY)
-		end
-		if (!@caption.empty? && !@gimage_caption.nil?)
-			#@gimage_caption.draw(@x + (@width - @gimage_caption.width) / 2, @y + (height - @gimage_caption.height) / 2, z + 1)
-		end
 	end
 
   def self.finalize(object_id)
--- scene-intro.rb
+++ scene-intro.rb
@@ -2,21 +2,16 @@ class SceneIntro
 	def initialize(window)
 		@window = window
 		@background = Gosu::Image.new("img/background.png")
-		@button = [Gosu::Image.new("img/button-start0.png"), Gosu::Image.new("img/button-start1.png")]
-		@x, @y, @width, @height = 96, 160, 128, 48
-		#@start_button = Button.new(@window, 128, 48, "Start")
-		#@start_button.x = 96
-		#@start_button.y = 160
-		
+		@button_start = Button.new(@window, 128, 48)
+		@button_start.x = 96
+		@button_start.y = 160
+		@button_start.set_image(0, Gosu::Image.new("img/button-start0.png"))
+		@button_start.set_image(1, Gosu::Image.new("img/button-start1.png"))
 	end
 
 	def draw
 		@background.draw(0, 0, 0)
-	    if @window.mouse_x >= @x && @window.mouse_x < @x + @width && @window.mouse_y >= @y && @window.mouse_y < @y + @height
-	      @button[1].draw(96, 160, 1)
-	    else
-	      @button[0].draw(96, 160, 1)
-	    end
+		@button_start.draw
 	end
 
 	def update
