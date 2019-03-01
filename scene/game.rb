8592feb7207e160ada3a8ba1a5c0ed53cccf719e
Jubin Park <jubin-park@naver.com>
2019-03-01 23:44:40 +0900

    revise 0-9 image

--- scene/game.rb
+++ scene/game.rb
@@ -4,14 +4,14 @@ class Scene
     	@window = window
     	@background = Gosu::Image.new("img/background.png")
       @hole = Array[Gosu::Image.new "img/hole.png"] * 4
-      @numbers = Array.new(10)
-      10.times {|i| @numbers[i] = Gosu::Image.from_text("#{i}", 24)}
+      @numbers = Gosu::Image.new("img/numbers.png")
+      @num = Array.new(10) {|index| @numbers.subimage(16*index, 0, 16, 16)}
     end
 
     def draw
     	@background.draw(0, 0, 0)
       @hole.each_index {|i| @hole[i].draw(88 + i * 36, 64, 1)}
-      @numbers.each_index{|i| @numbers[i].draw(40 + i * 24, 160, 2)}
+      @num.each_index{|i| @num[i].draw(40 + i * 24, 160, 2)}
     end
 
     def update
