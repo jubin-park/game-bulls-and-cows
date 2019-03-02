a0e4c7b947b175cb8307c09a5e4d79683801949d
Jubin Park <jubin-park@naver.com>
2019-03-02 23:45:13 +0900

    Identation

--- number-ball.rb
+++ number-ball.rb
@@ -1,29 +1,29 @@
 class NumberBall
-	attr_accessor :x, :y
+  attr_accessor :x, :y
 
-	COLOR_EASYMED = Gosu::Color.argb(0xFF45B649)
+  COLOR_EASYMED = Gosu::Color.argb(0xFF45B649)
 
-	def initialize(window, x, y, z, index)
-		@window = window
-		@@numbers ||= Gosu::Image.new("img/numbers.png")
+  def initialize(window, x, y, z, index)
+    @window = window
+    @@numbers ||= Gosu::Image.new("img/numbers.png")
 
-		@x = x
-		@y = y
-		@z = z
-		@index = index
+    @x = x
+    @y = y
+    @z = z
+    @index = index
 
-		@num = @@numbers.subimage(16*@index, 0, 16, 16)
-		@circle = Gosu::Image.new("img/circle16.png")
-	end
+    @num = @@numbers.subimage(16*@index, 0, 16, 16)
+    @circle = Gosu::Image.new("img/circle16.png")
+  end
 
-	def draw
-		@circle.draw(@x, @y, @z, 1, 1, COLOR_EASYMED)
-		@num.draw(@x, @y, @z)
-	end
+  def draw
+    @circle.draw(@x, @y, @z, 1, 1, COLOR_EASYMED)
+    @num.draw(@x, @y, @z)
+  end
 
-	def update
+  def update
 
-	end
+  end
 
   def under_mouse?
     return @window.mouse_x >= @x && @window.mouse_x < @x + @width && @window.mouse_y >= @y && @window.mouse_y < @y + @height
--- scene/game.rb
+++ scene/game.rb
@@ -1,15 +1,15 @@
 class Scene
-	class Game
+  class Game
     
-  	def initialize(window)
-    	@window = window
-    	@background = Gosu::Image.new("img/background.png")
+    def initialize(window)
+      @window = window
+      @background = Gosu::Image.new("img/background.png")
       @hole = Array[Gosu::Image.new "img/hole.png"] * 4
       @number_ball = Array.new(10) {|i| NumberBall.new(window, 40 + i * 24, 100, 3, i)}
     end
 
     def draw
-    	@background.draw(0, 0, 0)
+      @background.draw(0, 0, 0)
       @hole.each_index {|i| @hole[i].draw(88 + i * 36, 40, 1)}
       @number_ball.each{|ball| ball.draw}
       Gosu.draw_rect(24, 176, 272, 120, Gosu::Color.argb(128, 0, 0, 0), 0)
