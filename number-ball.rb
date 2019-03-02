ccf68fd320f0a7b467bd764fbeb58b9b65dc2f20
Jubin Park <jubin-park@naver.com>
2019-03-02 23:52:51 +0900

    Add CONST DIAMETER

--- number-ball.rb
+++ number-ball.rb
@@ -1,31 +1,33 @@
 class NumberBall
   attr_accessor :x, :y
+  attr_reader   :index
 
   COLOR_EASYMED = Gosu::Color.argb(0xFF45B649)
+  DIAMETER = 16
 
   def initialize(window, x, y, z, index)
     @window = window
     @@numbers ||= Gosu::Image.new("img/numbers.png")
-
+    @@circle ||= Gosu::Image.new("img/circle16.png")
     @x = x
     @y = y
     @z = z
     @index = index
-
-    @num = @@numbers.subimage(16*@index, 0, 16, 16)
-    @circle = Gosu::Image.new("img/circle16.png")
+    @num = @@numbers.subimage(@index * DIAMETER, 0, DIAMETER, DIAMETER)
   end
 
   def draw
-    @circle.draw(@x, @y, @z, 1, 1, COLOR_EASYMED)
+    @@circle.draw(@x, @y, @z, 1, 1, COLOR_EASYMED)
     @num.draw(@x, @y, @z)
   end
 
   def update
-
+    if under_mouse?
+      p @index
+    end
   end
 
   def under_mouse?
-    return @window.mouse_x >= @x && @window.mouse_x < @x + @width && @window.mouse_y >= @y && @window.mouse_y < @y + @height
+    return @window.mouse_x >= @x && @window.mouse_x < @x + DIAMETER && @window.mouse_y >= @y && @window.mouse_y < @y + DIAMETER
   end
 end
