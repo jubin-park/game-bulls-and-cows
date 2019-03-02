4d6c625a712a7c8644b818d4367c88f18abdff02
Jubin Park <jubin-park@naver.com>
2019-03-02 22:49:33 +0900

    add circle16.png

--- scene/game.rb
+++ scene/game.rb
@@ -6,12 +6,14 @@ class Scene
       @hole = Array[Gosu::Image.new "img/hole.png"] * 4
       @numbers = Gosu::Image.new("img/numbers.png")
       @num = Array.new(10) {|index| @numbers.subimage(16*index, 0, 16, 16)}
+      @circle = Array.new(10) {|index| Gosu::Image.new("img/circle16.png")}
     end
 
     def draw
     	@background.draw(0, 0, 0)
       @hole.each_index {|i| @hole[i].draw(88 + i * 36, 64, 1)}
-      @num.each_index{|i| @num[i].draw(40 + i * 24, 160, 2)}
+      @circle.each_index{|i| @circle[i].draw(40 + i * 24, 160, 2, 1, 1, Gosu::Color.argb(0xFF45B649))}
+      @num.each_index{|i| @num[i].draw(40 + i * 24, 160, 3)}
     end
 
     def update
