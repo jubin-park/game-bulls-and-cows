ce369642de1e5dc1fb44af0fb005f3adf983b107
Jubin Park <jubin-park@naver.com>
2019-03-04 23:55:47 +0900

    Add NumberBall Pickup

--- number-ball.rb
+++ number-ball.rb
@@ -9,28 +9,50 @@ class NumberBall
     @@numbers ||= Gosu::Image.new("img/numbers.png")
     @@circle  ||= Gosu::Image.new("img/circle16.png")
     @window = window
-    @x = x
-    @y = y
+    @nx = @x = x
+    @ny = @y = y
     @z = z
     @index = index
     @num = @@numbers.subimage(@index * DIAMETER, 0, DIAMETER, DIAMETER)
     @color_easymed = COLOR_EASYMED.dup
+
+    @picked = false
+
+    @mouse_sx = @mouse_sy = nil
   end
 
   def draw
     @@circle.draw(@x, @y, @z, 1, 1, @color_easymed)
-    @num.draw(@x, @y, @z)
+    @num.draw(@nx, @ny, @z)
   end
 
   def update
-    if under_mouse?
+    if under_mouse?(@x, @y)
       @color_easymed.alpha += 16
     else
       @color_easymed.alpha -= 8
     end
+    
+    # 집지 않은 상태
+    if @picked == false
+      # 숫자 영역에 마우스가 올려질 때
+      if under_mouse?(@nx, @ny)
+        # 마우스를 누를 때
+        if Gosu.button_down?(Gosu::MS_LEFT)
+          @picked = true
+        end
+      end
+    # 집은 상태
+    else
+      @nx = @window.mouse_x - DIAMETER / 2
+      @ny = @window.mouse_y - DIAMETER / 2
+      if Gosu.button_down?(Gosu::MS_LEFT)
+        @picked = false
+      end
+    end
   end
 
-  def under_mouse?
-    return @window.mouse_x >= @x && @window.mouse_x < @x + DIAMETER && @window.mouse_y >= @y && @window.mouse_y < @y + DIAMETER && @@circle[@window.mouse_x-@x, @window.mouse_y-@y] != "\0\0\0\0"
+  def under_mouse?(x, y)
+    return @window.mouse_x >= x && @window.mouse_x < x + DIAMETER && @window.mouse_y >= y && @window.mouse_y < y + DIAMETER && @@circle[@window.mouse_x-x, @window.mouse_y-y] != "\0\0\0\0"
   end
 end
